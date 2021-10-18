f66b319ef8c73d1ee86459d91e8628d412bfb844
中島真一 <nakajimabb@gmail.com>
2021-10-18 12:44:44 +0900

    firebase init

---
+++ src/firebase.ts
@@ -0,0 +1,8 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import { firebaseConfig } from './firebaseConfig';
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
