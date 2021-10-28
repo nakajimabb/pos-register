0cfc9c4c25a53a1a9b53e4a32c5ec146b0ee864f
Chihaya Taira <prelude371@gmail.com>
2021-10-28 18:34:53 +0900

    レシート印刷機能（プロトタイプ）

--- src/RegisterMain.tsx
+++ src/RegisterMain.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { getFirestore, doc, getDoc } from 'firebase/firestore';
 
-import { Button, Card, Flex, Form, Icon, Table } from './components';
+import { Button, Card, Flex, Form, Grid, Icon, Table } from './components';
 import RegisterPayment from './RegisterPayment';
 import { Product } from './types';
 
@@ -69,99 +69,105 @@ const RegisterMain: React.FC = () => {
           document.getElementById('productCode')?.focus();
         }}
       />
-      <Form className="" onSubmit={handleSubmit}>
-        <Form.Text
-          id="productCode"
-          size="md"
-          placeholder="商品コード"
-          className="mb-3 sm:mb-0"
-          value={productCode}
-          onChange={(e) => setProductCode(e.target.value)}
-        />
-      </Form>
-      <Card className="m-8 w-2/3">
-        <Card.Body className="p-4 bg-gray-50">
-          <Table border="row" className="table-fixed w-full">
-            <Table.Head>
-              <Table.Row>
-                <Table.Cell type="th" className="w-1/12">
-                  No.
-                </Table.Cell>
-                <Table.Cell type="th" className="w-2/12">
-                  コード
-                </Table.Cell>
-                <Table.Cell type="th" className="w-5/12">
-                  商品名
-                </Table.Cell>
-                <Table.Cell type="th" className="w-1/12">
-                  単価
-                </Table.Cell>
-                <Table.Cell type="th" className="w-1/12">
-                  数量
-                </Table.Cell>
-                <Table.Cell type="th" className="w-1/12" />
-              </Table.Row>
-            </Table.Head>
-            <Table.Body>
-              {basketItems?.map((basketItem, index) => (
-                <Table.Row key={index}>
-                  <Table.Cell>{index + 1}</Table.Cell>
-                  <Table.Cell>{basketItem.product.code}</Table.Cell>
-                  <Table.Cell>{basketItem.product.name}</Table.Cell>
-                  <Table.Cell className="text-right">
-                    ¥{basketItem.product.price?.toLocaleString()}
-                  </Table.Cell>
-                  <Table.Cell className="text-right">
-                    {basketItem.quantity}
-                  </Table.Cell>
-                  <Table.Cell className="text-center">
-                    <Button
-                      variant="icon"
-                      size="xs"
-                      color="none"
-                      className="hover:bg-gray-300"
-                      onClick={(e) => {
-                        setBasketItems(
-                          basketItems.filter(
-                            (item) =>
-                              item.product.code !== basketItem.product.code
-                          )
-                        );
-                      }}
+      <Grid cols="2" rows="1" gap="0" flow="col" className="h-full">
+        <Card className="container justify-center m-2">
+          <Card.Body>
+            <Form className="mt-16 ml-4" onSubmit={handleSubmit}>
+              <Form.Text
+                id="productCode"
+                size="md"
+                placeholder="商品コード"
+                className="mb-3 sm:mb-0"
+                value={productCode}
+                onChange={(e) => setProductCode(e.target.value.trim())}
+              />
+            </Form>
+
+            <div className="mt-8 h-96 overflow-y-scroll">
+              <Table border="row" className="table-fixed w-full text-sm ">
+                <Table.Head>
+                  <Table.Row>
+                    <Table.Cell type="th" className="w-1/12">
+                      No.
+                    </Table.Cell>
+                    <Table.Cell type="th" className="w-2/12">
+                      コード
+                    </Table.Cell>
+                    <Table.Cell type="th" className="w-5/12">
+                      商品名
+                    </Table.Cell>
+                    <Table.Cell type="th" className="w-1/12">
+                      単価
+                    </Table.Cell>
+                    <Table.Cell type="th" className="w-1/12">
+                      数量
+                    </Table.Cell>
+                    <Table.Cell type="th" className="w-1/12" />
+                  </Table.Row>
+                </Table.Head>
+                <Table.Body>
+                  {basketItems?.map((basketItem, index) => (
+                    <Table.Row
+                      key={index}
+                      className={index % 2 === 1 ? 'bg-blue-50' : ''}
                     >
-                      <Icon name="trash" />
-                    </Button>
+                      <Table.Cell>{index + 1}</Table.Cell>
+                      <Table.Cell>{basketItem.product.code}</Table.Cell>
+                      <Table.Cell>{basketItem.product.name}</Table.Cell>
+                      <Table.Cell className="text-right">
+                        ¥{basketItem.product.price?.toLocaleString()}
+                      </Table.Cell>
+                      <Table.Cell className="text-right">
+                        {basketItem.quantity}
+                      </Table.Cell>
+                      <Table.Cell className="text-center">
+                        <Button
+                          variant="icon"
+                          size="xs"
+                          color="none"
+                          className="hover:bg-gray-300"
+                          onClick={(e) => {
+                            setBasketItems(
+                              basketItems.filter(
+                                (item) =>
+                                  item.product.code !== basketItem.product.code
+                              )
+                            );
+                          }}
+                        >
+                          <Icon name="trash" />
+                        </Button>
+                      </Table.Cell>
+                    </Table.Row>
+                  ))}
+                </Table.Body>
+              </Table>
+            </div>
+
+            <Table border="row" className="table-fixed w-2/3 mt-8">
+              <Table.Body>
+                <Table.Row>
+                  <Table.Cell type="th" className="text-xl bg-red-100">
+                    合計
+                  </Table.Cell>
+                  <Table.Cell className="text-right text-xl pr-4">
+                    ¥
+                    {basketItems
+                      .reduce(
+                        (result, item) =>
+                          result + Number(item.product.price) * item.quantity,
+                        0
+                      )
+                      .toLocaleString()}
                   </Table.Cell>
                 </Table.Row>
-              ))}
-            </Table.Body>
-          </Table>
-        </Card.Body>
-      </Card>
+              </Table.Body>
+            </Table>
+          </Card.Body>
+        </Card>
 
-      <Card className="m-8 w-1/2">
-        <Card.Body className="p-4 bg-gray-50">
-          <Table border="row" className="table-fixed w-full">
-            <Table.Body>
-              <Table.Row>
-                <Table.Cell type="th" className="text-xl bg-red-100">
-                  合計
-                </Table.Cell>
-                <Table.Cell className="text-right text-xl pr-4">
-                  ¥
-                  {basketItems
-                    .reduce(
-                      (result, item) =>
-                        result + Number(item.product.price) * item.quantity,
-                      0
-                    )
-                    .toLocaleString()}
-                </Table.Cell>
-              </Table.Row>
-            </Table.Body>
-          </Table>
-        </Card.Body>
-      </Card>
+        <Card className="m-2"></Card>
+      </Grid>
     </Flex>
   );
 };
--- src/RegisterPayment.tsx
+++ src/RegisterPayment.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { useReactToPrint } from 'react-to-print';
 import { Button, Form, Modal, Table } from './components';
 import { Product } from './types';
 
@@ -15,6 +16,17 @@ type Props = {
 
 const RegisterPayment: React.FC<Props> = ({ open, basketItems, onClose }) => {
   const [cash, setCash] = useState<number>(0);
+  const componentRef = useRef(null);
+  const pageStyle = `
+    @media print {
+      @page { size: JIS-B5 portrait; }
+    }  
+  `;
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+    pageStyle,
+    onAfterPrint: onClose,
+  });
 
   useEffect(() => {
     document.getElementById('inputCash')?.focus(); //非推奨
@@ -63,6 +75,12 @@ const RegisterPayment: React.FC<Props> = ({ open, basketItems, onClose }) => {
                     onChange={(e) =>
                       setCash(Number(e.target.value.replace(/\D/, '')))
                     }
+                    onKeyPress={(e) => {
+                      if (e.key === 'Enter' && handlePrint) {
+                        e.preventDefault();
+                        handlePrint();
+                      }
+                    }}
                     className="text-right w-full"
                   />
                 </Table.Cell>
@@ -83,6 +101,90 @@ const RegisterPayment: React.FC<Props> = ({ open, basketItems, onClose }) => {
               </Table.Row>
             </Table.Body>
           </Table>
+          <div className="hidden">
+            <div ref={componentRef} className="p-10">
+              <p className="text-center">領収書</p>
+              <Table border="row" className="table-fixed w-full text-sm ">
+                <Table.Head>
+                  <Table.Row>
+                    <Table.Cell type="th" className="w-1/12" />
+                    <Table.Cell type="th" className="w-5/12">
+                      商品名
+                    </Table.Cell>
+                    <Table.Cell type="th" className="w-1/12">
+                      数量
+                    </Table.Cell>
+                    <Table.Cell type="th" className="w-1/12">
+                      単価
+                    </Table.Cell>
+                    <Table.Cell type="th" className="w-1/12">
+                      金額
+                    </Table.Cell>
+                  </Table.Row>
+                </Table.Head>
+                <Table.Body>
+                  {basketItems?.map((basketItem, index) => (
+                    <Table.Row key={index}>
+                      <Table.Cell></Table.Cell>
+                      <Table.Cell>{basketItem.product.name}</Table.Cell>
+                      <Table.Cell className="text-right">
+                        {basketItem.quantity}
+                      </Table.Cell>
+                      <Table.Cell className="text-right">
+                        ¥{basketItem.product.price?.toLocaleString()}
+                      </Table.Cell>
+                      <Table.Cell className="text-center">
+                        ¥
+                        {(
+                          Number(basketItem.product.price) * basketItem.quantity
+                        )?.toLocaleString()}
+                      </Table.Cell>
+                    </Table.Row>
+                  ))}
+                </Table.Body>
+              </Table>
+
+              <Table border="row" className="table-fixed w-full">
+                <Table.Body>
+                  <Table.Row>
+                    <Table.Cell type="th" className="text-xl">
+                      合計
+                    </Table.Cell>
+                    <Table.Cell className="text-right text-xl pr-4">
+                      ¥
+                      {basketItems
+                        .reduce(
+                          (result, item) =>
+                            result + Number(item.product.price) * item.quantity,
+                          0
+                        )
+                        .toLocaleString()}
+                    </Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell type="th">お預かり</Table.Cell>
+                    <Table.Cell className="text-right pr-4">
+                      ¥{cash.toLocaleString()}
+                    </Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell type="th">お釣り</Table.Cell>
+                    <Table.Cell className="text-right pr-4">
+                      ¥
+                      {(
+                        cash -
+                        basketItems.reduce(
+                          (result, item) =>
+                            result + Number(item.product.price) * item.quantity,
+                          0
+                        )
+                      ).toLocaleString()}
+                    </Table.Cell>
+                  </Table.Row>
+                </Table.Body>
+              </Table>
+            </div>
+          </div>
         </Modal.Body>
         <Modal.Footer className="flex justify-end">
           <Button
@@ -93,7 +195,9 @@ const RegisterPayment: React.FC<Props> = ({ open, basketItems, onClose }) => {
           >
             キャンセル
           </Button>
-          <Button color="primary">完了</Button>
+          <Button onClick={handlePrint} color="primary">
+            レシート発行
+          </Button>
         </Modal.Footer>
       </Form>
     </Modal>
