c24d3d3ce43781b95fe42b291c662b2da0cabe9b
中島真一 <>
2022-02-16 12:40:48 +0900

    tailwind.config.js => safelist を追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   purge: {
     content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
     options: {
-      safelist: [/^bg-/, /^text-/, /^hover:bg-/, /^hover:text-/, /grid-/, /border-/, /^gap-/, /^w-/, /^h-/, /ring/, /flex/ ], 
+      safelist: [/^bg-/, /^text-/, /^hover:bg-/, /^hover:text-/, /grid-/, /border-/, /^gap-/, /^w-/, /^h-/, /^m-/, /^p-/, /ring/, /flex/, /sm:/, /md:/, /lg:/, /xl:/ ], 
     },
   },    
   darkMode: false, // or 'media' or 'class'
